f83dd7f110adc37c59ebedec5abe33782214fd72
Yakoub <ouardi.yakoubabdallah@gmail.com>
2021-12-07 12:01:32 +0100

    Split the code to support services, middleware, controllers and models

---
+++ src/users/middleware/users.middleware.ts
@@ -0,0 +1,68 @@
+import { Request, Response, NextFunction } from 'express';
+import userService from '../services/users.service';
+import debug from 'debug';
+
+const log: debug.IDebugger = debug('app:users-controller');
+
+class UsersMiddleware {
+	async validateRequiredFields(req: Request, res: Response, next: NextFunction) {
+		if (req.body && req.body.email && req.body.password) {
+			next();
+		} else {
+			res.status(400).send({
+				error: `Missing required fields email and password`,
+			});
+		}
+	}
+
+	async validateEmailExistence(req: Request, res: Response, next: NextFunction) {
+		const user = await userService.getByEmail(req.body.email);
+		if (user) {
+			res.status(400).send({ error: `User email already exists` });
+		} else {
+			next();
+		}
+	}
+
+	async validateEmailForUser(req: Request, res: Response, next: NextFunction) {
+		const user = await userService.getByEmail(req.body.email);
+		if (user && user.id === req.params.userId) {
+			next();
+		} else {
+			res.status(400).send({ error: `Invalid email` });
+		}
+	}
+
+	// Here we need to use an arrow function to bind `this` correctly
+	validatePatchEmail = async (
+		req: Request,
+		res: Response,
+		next: NextFunction
+	) => {
+		if (req.body.email) {
+			log('Validating email', req.body.email);
+
+			this.validateEmailForUser(req, res, next);
+		} else {
+			next();
+		}
+	};
+
+	async validateUserExists(req: Request, res: Response, next: NextFunction) {
+		const user = await userService.getById(req.params.userId);
+		if (user) {
+			next();
+		} else {
+			res.status(404).send({
+				error: `User ${req.params.userId} not found`,
+			});
+		}
+	}
+
+	async extractUserId(req: Request, res: Response, next: NextFunction) {
+		req.body.id = req.params.userId;
+		next();
+	}
+}
+
+export default new UsersMiddleware();
--- src/users/users.routes.config.ts
+++ src/users/users.routes.config.ts
@@ -1,5 +1,7 @@
 import { CommonRoutesConfig } from '../common/common.routes.config';
 import express from 'express';
+import usersController from './controllers/users.controller';
+import usersMiddleware from './middleware/users.middleware';
 
 export class UsersRoutes extends CommonRoutesConfig {
 	public constructor(app: express.Application) {
@@ -9,40 +11,36 @@ export class UsersRoutes extends CommonRoutesConfig {
 	configureRoutes() {
 		this.app
 			.route('/users')
-			.get((req: express.Request, res: express.Response) => {
-				res.status(200).send(`List of users`);
-			})
-			.post((req: express.Request, res: express.Response) => {
-				res.status(200).send(`Post to users`);
-			});
+			.get(usersController.listUsers)
+			.post(
+				[
+					usersMiddleware.validateRequiredFields,
+					usersMiddleware.validateEmailExistence,
+				],
+				usersController.createUser
+			);
 
+		this.app.param('userId', usersMiddleware.extractUserId);
 		this.app
-			.route(`/users/:userId`)
-			.all(
-				(
-					req: express.Request,
-					res: express.Response,
-					next: express.NextFunction
-				) => {
-					// this middleware function runs before any request to /users/:userId
-					// but it doesn't accomplish anything just yet---
-					// it simply passes control to the next applicable function below using next()
-					// This function can be beneficial when we create routes that are meant to be accessed only by authenticated users.
-					next();
-				}
-			)
-			.get((req: express.Request, res: express.Response) => {
-				res.status(200).send(`GET requested for id ${req.params.userId}`);
-			})
-			.put((req: express.Request, res: express.Response) => {
-				res.status(200).send(`PUT requested for id ${req.params.userId}`);
-			})
-			.patch((req: express.Request, res: express.Response) => {
-				res.status(200).send(`PATCH requested for id ${req.params.userId}`);
-			})
-			.delete((req: express.Request, res: express.Response) => {
-				res.status(200).send(`DELETE requested for id ${req.params.userId}`);
-			});
+			.route('/users/:userId')
+			.all(usersMiddleware.validateUserExists)
+			.get(usersController.getUserById)
+			.delete(usersController.removeUser);
+
+		this.app.put(
+			'/users/:userId',
+			[
+				usersMiddleware.validateRequiredFields,
+				usersMiddleware.validateEmailForUser,
+			],
+			usersController.put
+		);
+
+		this.app.patch(
+			'/users/:userId',
+			[usersMiddleware.validatePatchEmail],
+			usersController.patch
+		);
 
 		return this.app;
 	}
